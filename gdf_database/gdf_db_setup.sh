8b2003989bc77f6a8776d81ef5eb17253f872378
Jeremy Hooke <jez@stulk.com>
2015-10-23 11:44:51 +1100

    Make the DB create script output more readable.

--- gdf_database/gdf_db_setup.sh
+++ gdf_database/gdf_db_setup.sh
@@ -6,8 +6,10 @@ set -eu
 dbname=gdf_empty
 admin_user=cube_admin
 
+psql='psql -q'
+
 # Create default GDF groups and users (ignore failures: they may already exist)
-psql -c "
+${psql} -d postgres -e -c "
 CREATE ROLE cube_admin_group
   NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
 
@@ -19,7 +21,7 @@ GRANT cube_admin_group TO ${admin_user};
 
 
 # Create new database ${dbname}
-psql -d postgres -c "
+${psql} -e -d postgres -c "
 CREATE DATABASE ${dbname}
   WITH OWNER = cube_admin_group
        TEMPLATE = template0
@@ -29,23 +31,27 @@ CREATE DATABASE ${dbname}
        LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1;"
 
-psql -d ${dbname} -c "
+${psql} -e -d ${dbname} -c "
 ALTER DATABASE ${dbname}
   SET search_path = "\"$USER\"", public, topology;"
 
 # Install required extensions to database
-psql -d ${dbname} -f extensions.sql
+${psql} -e -d ${dbname} -f extensions.sql
 
+echo
 # Apply each change in order. 
 # TODO: Temporary solution: it doesn't hanlde multi-digit version numbers.
 #       We will probably use a tool like Flyway for real deployments.
 for f in ./versioned/v*.sql;
 do
-    psql -d ${dbname} -v ON_ERROR_STOP=1 --single-transaction <<EOF
+    echo -n "Running ${f}..."
+    ${psql} -d ${dbname} -v ON_ERROR_STOP=1 --single-transaction <<EOF
     set role to cube_admin_group;
     \\i ${f}
 EOF
+    echo 'done'
 done
+echo
 
-psql -d ${dbname} -f grants.sql
+${psql} -e -d ${dbname} -f grants.sql
 
