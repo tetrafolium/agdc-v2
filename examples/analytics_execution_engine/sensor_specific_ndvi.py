cdf9ceb6d85bf6a9ef705f5fb57a48f3fe278b18
Peter Wang <peter.wang@csiro.au>
2015-11-20 15:24:42 +1100

    examples of AE/EE from GDF trial and NDexpr

---
+++ examples/analytics_execution_engine/sensor_specific_ndvi.py
@@ -0,0 +1,44 @@
+# ------------------------------------------------------------------------------
+# Name:       sensor_specific_ndvi.py
+# Purpose:    sensor specific ndvi example for Analytics Engine & Execution Engine.
+#             pre-integration with NDExpr.
+#             Taken from the GDF Trial.
+#
+# Author:     Peter Wang
+#
+# Created:    20 November 2015
+# Copyright:  2015 Commonwealth Scientific and Industrial Research Organisation
+#             (CSIRO)
+# License:    This software is open source under the Apache v2.0 License
+#             as provided in the accompanying LICENSE file or available from
+#             https://github.com/data-cube/agdc-v2/blob/master/LICENSE
+#             By continuing, you acknowledge that you have read and you accept
+#             and will abide by the terms of the License.
+#
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import print_function
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
+from pprint import pprint
+from datetime import datetime
+from analytics import Analytics
+from execution_engine import ExecutionEngine
+from analytics_utils import plot
+
+def main():
+    a = Analytics()
+    e = ExecutionEngine()
+
+    dimensions = {'X': {'range': (147.0, 147.256)},
+                  'Y': {'range': (-37.0, -36.744)}}
+
+    ndvi = a.applySensorSpecificBandMath('LS5TM', 'ndvi', dimensions, 'get_data', 'ndvi')
+    result = e.executePlan(a.plan)
+
+    plot(e.cache['ndvi'])
+
+if __name__ == '__main__':
+    main()
---
+++ examples/ndexpr/ndvi_mask_median_reduction.py
@@ -0,0 +1,78 @@
+# ------------------------------------------------------------------------------
+# Name:       ndvi_mask_median_reduction.py
+# Purpose:    ndvi + mask + median reduction example for ndexpr
+#             pre-integration into Analytics Engine & Execution Engine.
+#
+# Author:     Peter Wang
+#
+# Created:    20 November 2015
+# Copyright:  2015 Commonwealth Scientific and Industrial Research Organisation
+#             (CSIRO)
+# License:    This software is open source under the Apache v2.0 License
+#             as provided in the accompanying LICENSE file or available from
+#             https://github.com/data-cube/agdc-v2/blob/master/LICENSE
+#             By continuing, you acknowledge that you have read and you accept
+#             and will abide by the terms of the License.
+#
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import print_function
+import os
+import sys
+import xray
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
+from gdf import GDF
+from ndexpr import NDexpr
+
+
+def main():
+    print('Instantiating GDF and NDexpr')
+    g = GDF()
+    nd = NDexpr()
+
+    print('Retrieving data from GDF')
+    # construct data request parameters for B30 and B40
+    data_request_descriptor = {'storage_type': 'LS5TM',
+                               'variables': ('B30', 'B40'),
+                               'dimensions': {'X': {'range': (147.0, 147.256)},
+                                              'Y': {'range': (-37.0, -36.744)}
+                                              }
+                               }
+
+    # get data
+    d1 = g.get_data(data_request_descriptor)
+
+    # construct data request parameters for PQ
+    pq_request_descriptor = {'storage_type': 'LS5TMPQ',
+                             'variables': ['PQ'],
+                             'dimensions': {'X': {'range': (147.0, 147.256)},
+                                            'Y': {'range': (-37.0, -36.744)}
+                                            }
+                             }
+
+    # get data
+    d2 = g.get_data(pq_request_descriptor)
+
+    # The following 3 lines shouldn't be done like this
+    # Currently done like this for the sake of the example.
+    b30 = xray.DataArray(d1['arrays']['B30'])
+    b40 = xray.DataArray(d1['arrays']['B40'])
+    pq = xray.DataArray(nd.get_pqa_mask(d2['arrays']['PQ']))
+
+    print('NDexpr demo begins here')
+    # ndvi + mask + median reduction example as expressed in this language.
+    ndvi = nd.evaluate('((b40 - b30) / (b40 + b30))')
+    masked_ndvi = nd.evaluate('ndvi{pq}')
+
+    # currently dimensions are integer indices, later will be labels when
+    # Data Access API Interface has been finalised.
+    reduction_on_dim0 = nd.evaluate('median(masked_ndvi, 0)')
+    reduction_on_dim01 = nd.evaluate('median(masked_ndvi, 0, 1)')
+    reduction_on_dim012 = nd.evaluate('median(masked_ndvi, 0, 1, 2)')
+    print(reduction_on_dim0)
+    print(reduction_on_dim01)
+    print(reduction_on_dim012)
+
+if __name__ == '__main__':
+    main()
